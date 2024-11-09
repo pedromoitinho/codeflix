85f71112a09d1885d82b6c9c46145c391853a92a
pedromoitinho <pedrohenriquemoitinho.prado@ucsal.edu.br>
2024-11-09 01:08:06 -0300

    pepe

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import fs from 'fs';
 import path from 'path';
 
+
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
+plugins: [svelte()],
+server: {
     watch: {
       usePolling: true,
       interval: 100,
